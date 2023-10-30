e2f538cbb62d53b50d6c128405eb30633c166fd5
ItsKristian01 <kristianhana11@gmail.com>
2023-10-30 16:36:44 +0100

    wip: click on cells

--- js/script.js
+++ js/script.js
@@ -1,32 +1,52 @@
+
 // Numbers generator
 for (let i = 1; i <= 100; i++) {
-  const divCol = document.createElement("div");   // Element Creation
+  const divCol = document.createElement("div"); // Element Creation
   divCol.classList.add("col"); // Added class
   divCol.innerHTML = i;
   console.log(divCol);
-  const element = document.getElementById("numbers"); 
+  const element = document.getElementById("numbers");
   element.append(divCol); // Added element to HTML
-  
-// Cell click activation
-  divCol.addEventListener("click", function(){
-    this.classList.toggle("color");
-    console.log(divCol);
-  })
 
-  const bombs = generateBombs(100);
-  const maxClicks = 100 - bombs.length;
-  console.log(bombs, maxClicks);
+  // Cell click activation
+  divCol.addEventListener("click", function () {
+    const bombs = generateBombs(100);
+    const maxClicks = 100 - bombs.length;
+    console.log(maxClicks);
+   
+    console.log(divCol);
+
+    if (divCol === bombs) {
+      divCol.classList.ada("red");
+      console.log("Hai perso");
+    } else {
+      divCol.classList.toggle("color");
+    }
+  });
 }
 
 // Button Click
 let buttonPlay = document.getElementById("button-play");
-buttonPlay.addEventListener("click", function() {   //Added Click to Button Play
-   const element = document.getElementById("numbers");
-   element.classList.remove("remove");
-})
+buttonPlay.addEventListener("click", function () {
+  //Added Click to Button Play
+  const element = document.getElementById("numbers");
+  element.classList.remove("remove");
+});
 
 
 
+
+/**
+ * Description placeholder
+ * @date 10/30/2023 - 4:34:58 PM
+ *
+ * @param {*} min
+ * @param {*} max
+ * @returns {*}
+ */
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 /**
  * Description placeholder
@@ -37,18 +57,10 @@ buttonPlay.addEventListener("click", function() {   //Added Click to Button Play
 function generateBombs(max) {
   const numbers = [];
   while (numbers.length < 16) {
-    const rndNumber = Math.floor(Math.random() * 16) + 1;
-    if(!numbers.includes(rndNumber)) {
+    const rndNumber = getRndInteger(1, max);
+    if (!numbers.includes(rndNumber)) {
       numbers.push(rndNumber);
     }
   }
   return numbers;
 }
-
-
-
-
-
-
-
-
